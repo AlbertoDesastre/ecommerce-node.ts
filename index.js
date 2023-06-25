26f37ba2e8367c381cfecbf672761dffcbae14b6
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-06-25 13:30:40 +0200

    added route to search for specific product

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/products', (req, res) => {
   const products = [];
   for (let index = 0; index < 100; index++) {
     products.push({
+      id: index,
       name: faker.commerce.product(),
       price: parseInt(faker.commerce.price(), 10),
       image: faker.image.imageUrl(),
@@ -14,6 +15,25 @@ app.get('/products', (req, res) => {
   }
 
   res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+
+  // I create random products everytime and then I pick up only the one who it's equal to the parameter
+  const products = [];
+  for (let index = 0; index < 100; index++) {
+    products.push({
+      id: index,
+      name: faker.commerce.product(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+
+  const searchedProduct = products.filter((product) => product.id == id);
+
+  res.json(searchedProduct);
 });
 
 app.get('/goodbye', (req, res) => {

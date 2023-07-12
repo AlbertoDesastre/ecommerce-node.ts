dc1ad71b038fa53f1eb6f25fb131cb43cda76e0b
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-07-12 19:09:38 +0200

    refactor all controllers to their file

--- src/components/products/controllers.ts
+++ src/components/products/controllers.ts
@@ -1,6 +1,10 @@
 import { ProductService } from "./services";
 import { success, errors } from "../../network";
 
+/* As a general concept, controllers and in charge of managing the entry and the exit of the routes.
+Controller analyze the request: if it's correct, if the body fills the rules, there are no weird things, etc...
+Logic of bussines is responsability of the service. For example, how the body that will be sent on the response will look like
+*/
 class ProductController {
   private productService;
 
@@ -30,6 +34,121 @@ class ProductController {
       })
       .catch((err) => {
         return res.status(500).json(err);
+      });
+  }
+
+  filterBy(req, res) {
+    /* REMINDER! What comes from params it's always a string */
+    const { name, price, color } = req.query;
+
+    /*   console.log(req.query); */
+
+    this.productService
+      .filterBy({ name, price, color })
+      .then((result: any) => {
+        if (result.length === 0) {
+          return errors({ res, message: "No product was found", status: 401 });
+        } else {
+          return success({
+            res,
+            message: "Product/s available...",
+            data: result,
+            status: 201,
+          });
+        }
+      })
+      .catch((err) => {
+        return errors({ res, message: err, status: 500 });
+      });
+  }
+
+  getOne(req, res) {
+    /* REMINDER! What comes from params it's always a string */
+    const { id } = req.params;
+
+    /* data expected to be received = array */
+    this.productService
+      .getOne({ id })
+      .then((result: any) => {
+        if (result.length === 0) {
+          return errors({ res, message: "No product was found", status: 401 });
+        } else {
+          return success({
+            res,
+            message: "This product is available",
+            data: result,
+            status: 201,
+          });
+        }
+      })
+      .catch((err) => {
+        return errors({ res, message: err, status: 500 });
+      });
+  }
+
+  create(req, res) {
+    if (Object.keys(req.body).length === 0) {
+      return errors({
+        res,
+        message: "You didn't provide a body",
+        status: 400,
+      });
+    }
+
+    this.productService
+      .create(req.body)
+      .then((result: any) => {
+        return success({
+          res,
+          message: "All product/s created",
+          data: result.message,
+          status: 201,
+        });
+      })
+      .catch((err) => {
+        return errors({ res, message: err, status: 500 });
+      });
+  }
+
+  update(req, res) {
+    if (Object.keys(req.body).length === 0) {
+      return errors({
+        res,
+        message: "You didn't provide a body",
+        status: 400,
+      });
+    }
+
+    this.productService
+      .update({ product: req.body })
+      .then((result: any) => {
+        return success({
+          res,
+          message: "The product was updated",
+          data: result.message,
+          status: 201,
+        });
+      })
+      .catch((err) => {
+        return errors({ res, message: err, status: 500 });
+      });
+  }
+
+  deactivateProduct(req, res) {
+    const { id } = req.params;
+
+    this.productService
+      .deactivateProduct({ id })
+      .then((result: any) => {
+        return success({
+          res,
+          message: "Product deactivated",
+          data: result.message,
+          status: 201,
+        });
+      })
+      .catch((err) => {
+        return errors({ res, message: err, status: 500 });
       });
   }
 }
--- src/components/products/routes.ts
+++ src/components/products/routes.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import { ProductService } from "./services";
 import { ProductController } from "./controllers";
-import { success, errors } from "../../network";
 
 const router = express.Router();
 
-const productsService = new ProductService();
 const productController = new ProductController();
 
 router.get("/", (req, res) => {
@@ -13,119 +10,24 @@ router.get("/", (req, res) => {
 });
 
 router.get("/filter", (req, res) => {
-  /* REMINDER! What comes from params it's always a string */
-  const { name, price, color } = req.query;
-
-  /*   console.log(req.query); */
-
-  productsService
-    .filterBy({ name, price, color })
-    .then((result: any) => {
-      if (result.length === 0) {
-        return errors({ res, message: "No product was found", status: 401 });
-      } else {
-        return success({
-          res,
-          message: "Product/s available...",
-          data: result,
-          status: 201,
-        });
-      }
-    })
-    .catch((err) => {
-      return errors({ res, message: err, status: 500 });
-    });
+  productController.filterBy(req, res);
 });
 
 /* always put routes that requires dynamic data at the end, or the routs with fixed words won't be accesible */
 router.get("/:id", (req, res) => {
-  /* REMINDER! What comes from params it's always a string */
-  const { id } = req.params;
-
-  /* data expected to be received = array */
-  productsService
-    .getOne({ id })
-    .then((result: any) => {
-      if (result.length === 0) {
-        return errors({ res, message: "No product was found", status: 401 });
-      } else {
-        return success({
-          res,
-          message: "This product is available",
-          data: result,
-          status: 201,
-        });
-      }
-    })
-    .catch((err) => {
-      return errors({ res, message: err, status: 500 });
-    });
+  productController.getOne(req, res);
 });
 
 router.post("/", (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    return errors({
-      res,
-      message: "You didn't provide a body",
-      status: 400,
-    });
-  }
-
-  productsService
-    .create(req.body)
-    .then((result: any) => {
-      return success({
-        res,
-        message: "All product/s created",
-        data: result.message,
-        status: 201,
-      });
-    })
-    .catch((err) => {
-      return errors({ res, message: err, status: 500 });
-    });
+  productController.create(req, res);
 });
 
 router.put("/", (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    return errors({
-      res,
-      message: "You didn't provide a body",
-      status: 400,
-    });
-  }
-
-  productsService
-    .update({ product: req.body })
-    .then((result: any) => {
-      return success({
-        res,
-        message: "The product was updated",
-        data: result.message,
-        status: 201,
-      });
-    })
-    .catch((err) => {
-      return errors({ res, message: err, status: 500 });
-    });
+  productController.update(req, res);
 });
 
 router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-
-  productsService
-    .deactivateProduct({ id })
-    .then((result: any) => {
-      return success({
-        res,
-        message: "Product deactivated",
-        data: result.message,
-        status: 201,
-      });
-    })
-    .catch((err) => {
-      return errors({ res, message: err, status: 500 });
-    });
+  productController.deactivateProduct(req, res);
 });
 
 export { router };

9a11006318408f3c650e21f631afa14fac80834a
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-07-25 14:38:40 +0200

    add the first real test

--- test/products/services.test.ts
+++ test/products/services.test.ts
@@ -1,13 +1,19 @@
 import { ConnectionMethods } from "../../src/store/interfaces";
 import { handleConnection } from "../../src/store/mysql";
+import { ProductService } from "../../src/components/products/services";
+import { Product } from "../../src/components/products/interfaces";
 
 describe("*TEST* --> PRODUCTS__Service", () => {
   let connection: ConnectionMethods;
+  let productService: ProductService;
   beforeAll(() => {
     connection = handleConnection();
+    productService = new ProductService();
   });
 
-  test("should receive a list of products", async () => {
-    const products = connection;
+  test("should receive a list of 15 products", async () => {
+    const products = (await productService.list({})) as Product[];
+
+    expect(products.length).toBe(15);
   });
 });

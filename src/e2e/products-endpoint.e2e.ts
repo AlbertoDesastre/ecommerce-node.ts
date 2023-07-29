310ef5362eb567b329dc365f887a377d87e81a8f
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-07-29 12:38:16 +0200

    refactor integration test and added explanation to code

--- src/e2e/products-endpoint.e2e.ts
+++ src/e2e/products-endpoint.e2e.ts
@@ -5,15 +5,22 @@ const handleConnectionMock = {
   getOne: mockGetOne,
 };
 /* The 3 following lines were added just now. I have absolutely no idea why it's working now */
-const mysqlStore = jest.requireActual("../store/mysql");
+const mysqlStoreMock = jest.requireActual("../store/mysql");
 const handleConnection = jest.fn().mockReturnValue(handleConnectionMock);
-mysqlStore.handleConnection = handleConnection;
+mysqlStoreMock.handleConnection = handleConnection;
 import request from "supertest";
 import { Express } from "express";
 import http from "http";
 import { app } from "..";
 
-jest.mock("../store/mysql", () => mysqlStore);
+/* When calling the real mysqlStore, it will get substitute by "mysqlStoreMock". Now something really interesting happens here...
+The product service calls "mysqlStoreMock" that contains the function "handleConnection", exactly the same name the real module has.
+If you changed "handleConnection" name that's getting assigned on line 10, jest will try to Mock the Module, and when trying to access
+the method "handleConnection" it will fail. Jest needs the exact name the Product Service it's using in real cases, otherwises, it will fail.
+
+In fact, you could change the const in line 9 to whatever name you want and it will still work. The magic is happening in line 10, when the mock is getting
+assigned "handleConnection" with a the mocked function.*/
+jest.mock("../store/mysql", () => mysqlStoreMock);
 
 describe("Test for products endpoint", () => {
   let expressApp: Express;
@@ -31,7 +38,7 @@ describe("Test for products endpoint", () => {
   describe("test for [GET] /api/v1/products/", () => {
     test("should return an array with an object' ", async () => {
       //Arrange
-      mockList.mockReturnValue([
+      const sampleProduct = [
         {
           id: 11,
           category_id: 3,
@@ -45,14 +52,18 @@ describe("Test for products endpoint", () => {
           active: 1,
           created_at: "2023-07-09T18:57:14.000Z",
         },
-      ]);
+      ];
+      mockList.mockReturnValue(sampleProduct);
+
       //Act
       return await request(app)
         .get("/api/v1/products/")
         .expect(200)
-        .then((response: any) => {
+        .then((response: request.Response) => {
+          const responseBody = JSON.parse(response.text);
+
           //Assert
-          expect(JSON.parse(response.text).body.length).toEqual(1);
+          expect(responseBody.body.length).toEqual(1);
         });
     });
   });

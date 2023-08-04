89072d9802885be118c26ff2f15ed884960937c2
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-08-04 20:38:33 +0200

    .

--- src/components/products/services.ts
+++ src/components/products/services.ts
@@ -1,6 +1,6 @@
 import { MysqlError } from "mysql";
 
-import * as mysqlStore from "../../store/mysql";
+import { handleConnection } from "../../store/mysql";
 import { FilterQueries, Product } from "./interfaces";
 import { MysqlQueryResult } from "../../store/interfaces";
 
@@ -8,9 +8,8 @@ class ProductService {
   private connection;
 
   constructor() {
-    /* console.log("tipo del modulo -->", typeof mysqlStore);
-       console.log("cómo se ve el modulo -->", mysqlStore); */
-    this.connection = mysqlStore.handleConnection();
+    this.connection = handleConnection();
+    console.log(this.connection);
   }
 
   async list({ limit = "15", offset = "0" }) {

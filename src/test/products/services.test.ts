98d02d37da9b5b6f4a40fa2f6ce39c17c85a37df
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-08-07 17:17:48 +0200

    added test for arguments received on product list

--- src/test/products/services.test.ts
+++ src/test/products/services.test.ts
@@ -1,6 +1,6 @@
 const mockHandleConnection = jest.fn();
-const mockList = jest.fn();
-const mockGetOne = jest.fn();
+const mockSqlList = jest.fn();
+const mockSqlGetOne = jest.fn();
 
 import { ProductService } from "../../components/products/services";
 import { Product } from "../../components/products/interfaces";
@@ -9,52 +9,75 @@ import { fakeProducts } from "./assets";
 jest.mock("../../store/mysql", () => {
   return {
     handleConnection: mockHandleConnection.mockReturnValue({
-      list: mockList,
-      getOne: mockGetOne,
+      list: mockSqlList,
+      getOne: mockSqlGetOne,
     }),
   };
 });
 
-describe("*TEST* --> PRODUCTS__Service", () => {
+describe("test for Products Service", () => {
   let productService: ProductService;
+  let productListSpy: jest.SpyInstance;
 
   beforeAll(() => {
     productService = new ProductService();
+    productListSpy = jest.spyOn(productService, "list");
+  });
+
+  beforeEach(() => {
     /* This is specially useful when you want unaltered data for each test.  For example, If I wanted to sum "b = 2 + a" in test A, and I wanted to divide in Test B  "c = 2 / b" , the "b" will have the value modified by previous test.*/
     jest.clearAllMocks();
   });
 
-  test("should receive a list of 15 products", async () => {
-    mockList.mockReturnValue(fakeProducts);
-    const products = (await productService.list({})) as Product[];
+  describe("products calling [LIST]", () => {
+    test("should receive 'limit=15' and 'offset=0' when it's undefined from controller", async () => {
+      productService.list({});
 
-    expect(products.length).toBe(15);
-    expect(mockList).toHaveBeenCalled();
-    expect(mockList).toHaveBeenCalledTimes(1);
-    expect(mockList).toHaveBeenCalledWith({
-      limit: "15",
-      offset: "0",
-      table: "products",
+      expect(productListSpy).toHaveBeenCalledWith({});
+      expect(productListSpy).toHaveBeenCalledTimes(1);
+
+      /* remember that mockList is the mocked version of the mysqlStore, not the "list" method of productsService */
+      expect(mockSqlList).toHaveBeenCalledTimes(1);
+      expect(mockSqlList).toHaveBeenCalledWith({
+        limit: "15",
+        offset: "0",
+        table: "products",
+      });
     });
-  });
 
-  test("should receive a specific product, in position 0", async () => {
-    mockList.mockReturnValue([
-      {
-        id: 11,
-        category_id: 3,
-        name: "Nintendo Switch",
-        description:
-          "Versatile gaming console for both handheld and TV gaming.",
-        color: "Neon Red/Neon Blue",
-        price: 127,
-        quantity: 90,
-        image: "",
-        active: 1,
-        created_at: "2023-07-09T18:57:14.000Z",
-      },
-    ]);
-    const products = (await productService.list({})) as Product[];
-    expect(products[0].name).toEqual("Nintendo Switch");
+    test("should receive a list of 15 products", async () => {
+      mockSqlList.mockReturnValue(fakeProducts);
+      const products = (await productService.list({})) as Product[];
+
+      expect(productListSpy).toHaveBeenCalledTimes(1);
+      expect(products.length).toBe(15);
+      expect(mockSqlList).toHaveBeenCalled();
+      expect(mockSqlList).toHaveBeenCalledTimes(1);
+      expect(mockSqlList).toHaveBeenCalledWith({
+        limit: "15",
+        offset: "0",
+        table: "products",
+      });
+    });
+
+    test("should receive a specific product, in position 0", async () => {
+      mockSqlList.mockReturnValue([
+        {
+          id: 11,
+          category_id: 3,
+          name: "Nintendo Switch",
+          description:
+            "Versatile gaming console for both handheld and TV gaming.",
+          color: "Neon Red/Neon Blue",
+          price: 127,
+          quantity: 90,
+          image: "",
+          active: 1,
+          created_at: "2023-07-09T18:57:14.000Z",
+        },
+      ]);
+      const products = (await productService.list({})) as Product[];
+      expect(products[0].name).toEqual("Nintendo Switch");
+    });
   });
 });

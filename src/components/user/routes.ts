bcb0f37e5a92e48440985e14fc6b623be876716e
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-09-22 18:08:27 +0200

    changes in user routing and swagger info

--- src/components/user/routes.ts
+++ src/components/user/routes.ts
@@ -6,14 +6,10 @@ import * as UserController from "./controllers";
 let authMiddleware = new AuthMiddleware();
 const router = express.Router();
 
-router.get("/get/:id", UserController.get);
+router.get("/:id", UserController.get);
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
-router.put("/update/:id", authMiddleware.checkToken, UserController.update);
-router.delete(
-  "/delete/:id",
-  authMiddleware.checkToken,
-  UserController.eliminate
-);
+router.put("/:id", authMiddleware.checkToken, UserController.update);
+router.delete("/:id", authMiddleware.checkToken, UserController.eliminate);
 
 export { router };

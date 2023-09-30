2ee0571c03deabe01b708ccef41c3837ab83301d
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-09-30 14:04:02 +0200

    added test to delete functionality in products

--- src/e2e/products/products-controller.e2e.ts
+++ src/e2e/products/products-controller.e2e.ts
@@ -141,7 +141,7 @@ describe("Test for *PRODUCTS* --> CONTROLLER", () => {
     });
   });
 
-  describe("test for [LIST] (/api/v1/products/:productId -- GET) ", () => {
+  describe("test for [LIST] (/api/v1/products/ -- GET) ", () => {
     test("should return up to 15 products if no limit or offset it's passed", async () => {
       //Act
       return await request(app)
@@ -262,7 +262,7 @@ describe("Test for *PRODUCTS* --> CONTROLLER", () => {
     }); */
   });
 
-  describe("test for [FILTER-BY] (/api/v1/products/register -- GET) ", () => {
+  describe("test for [FILTER-BY] (/api/v1/products/filter -- GET) ", () => {
     test("should return 404 if none of the filters matchs any product", async () => {
       // Act
       return await request(app)
@@ -366,189 +366,80 @@ describe("Test for *PRODUCTS* --> CONTROLLER", () => {
         });
     });
   });
-  /*
-  describe("test for [REGISTER] (/api/v1/products/register -- POST) ", () => {
-    // Arrange
-    beforeEach(async () => {});
+
+  describe("test for [DEACTIVATE] (/api/v1/products/:productId -- DELETE) ", () => {
     afterEach(async () => {
-      connection.eliminate({ table: "users" });
+      await connection.eliminate({ table: "products" });
+      await connection.create({
+        table: "products",
+        tableColumns: ProductTableColumns.PRODUCTS_POST_VALUES_FOR_TEST,
+        arrayOfData: productsReadyToCreate,
+      });
     });
 
-    test("should return 400 if the attributes are undefined, or are present but are undefined or null", async () => {
-      //Act
+    test("should return 404 if product wasn't found", async () => {
+      // Act
       return await request(app)
-        .post("/api/v1/products/")
-        .send({
-          username: null,
-          email: undefined,
-          password: null,
-        })
-        .expect(400)
+        .delete("/api/v1/products/109571290")
+        .expect(404)
         .then((res) => {
           expect(JSON.parse(res.text)).toEqual({
-            body: "Username, email and password must be provided to register an user",
             error: true,
-            status: 400,
+            status: 404,
+            body: "No product was found",
           });
         });
     });
 
-    test("should create User if all data it's provided", async () => {
-      //Act
-      return await request(app)
-        .post("/api/v1/products/")
-        .send({
-          username: "eduardo",
-          email: "eduardo@mail.com",
-          password: "12345",
-        })
-        .expect(201)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: false,
-            status: 201,
-            message: "User created succesfully",
-            body: "Login using your password and user/email",
-          });
-        });
-    });
+    test("should return 500 if product didn't change it's state", async () => {
+      const productId: any = await connection.personalizedQuery(
+        `SELECT id FROM products WHERE name = '${productsReadyToCreate[0][0]}'`
+      );
+      const { id } = productId[0];
 
-    test("should throw 'existing user' error if user with the same 'email' or 'username' exists ", async () => {
-      // first call registers userTemplate
+      // first the product gets deactivated
       await request(app)
-        .post("/api/v1/products/")
-        .send()
-        .expect(201)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: false,
-            status: 201,
-            message: "User created succesfully",
-            body: "Login using your password and user/email",
-          });
-        });
-      // second call registers the same userTemplate
-      return await request(app)
-        .post("/api/v1/products/")
-        .send()
-        .expect(409)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: true,
-            status: 409,
-            body: null,
-          });
-        });
-    });
-  });
-
-  describe("test for [UPDATE] (/api/v1/products/:id -- PUT)", () => {
-    let user;
-    let token: string;
-    let userIdInArray: any;
-    let userId: string;
-
-    const userGettingUpdated = {
-      username: "paco",
-      email: "paco@mail.com",
-      avatar: "url_fake/e.com",
-      password: "54321",
-    };
-
-    // Arrange
-    beforeEach(async () => {
-      // user = await productService.create(userTemplate);
-    });
-    afterEach(async () => {
-      connection.eliminate({ table: "users" });
-    });
-
-    test("should return 400 if no token was provided", async () => {
-      // Act
-      return await request(app)
-        .put(`/api/v1/products/`)
-        .set({})
-        .send()
-        .expect(401)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: true,
-            status: 401,
-            body: ErrorThrower.PRODUCT_COULDNT_UPDATE,
-          });
-        });
-    });
-
-    test("should return 400 if token was provided with wrong format", async () => {
-      // Act
-      return await request(app)
-        .put(`/api/v1/products/`)
-        .set({ Authorization: `GWBIOG ${token}` })
-        .send()
-        .expect(401)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: true,
-            status: 401,
-            body: ErrorThrower.PRODUCT_COULDNT_UPDATE,
-          });
-        });
-    });
-
-    test("should return 500 if a user tries to update another user", async () => {
-      // Act
-      return await request(app)
-        .put(`/api/v1/products/${userId}`) // this is the id from the TemplateUser, not Desastre's User
-        .set({})
-        .send()
-        .expect(500)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: true,
-            status: 500,
-            body: null,
-          });
-        });
-    });
-
-    test("should return 201 if user is updated successfully", async () => {
-      return await request(app)
-        .put(`/api/v1/products/${userId}`)
-        .set({ Authorization: `Bearer ${token}` })
-        .send(userGettingUpdated)
-        .expect(201)
-        .then((res) => {
-          expect(JSON.parse(res.text)).toEqual({
-            error: false,
-            status: 201,
-            message: "Your profile was updated succesfully",
-            body: SuccessfulQueryMessage.ITEM_WAS_UPDATED,
-          });
-        });
-    });
-  });
-
-  describe("test for [DELETE] (/api/v1/products/:id -- DELETE)", () => {
-    test("should return 200 if user information it's deleted", async () => {
-      // pending to create orders and order items for this user to delete all info
-      // also pending to manage cases where user doesn't have any orders yet and those deletions have to be omiited
-
-      // await productService.create(product);
-
-      // const { id } = desastreIdInArray[0];
-
-      return await request(app)
-        .delete(`/api/v1/products/`)
-        .set({})
+        .delete(`/api/v1/products/${id}`)
         .expect(200)
-        .then((res) => {
+        .then(async (res) => {
           expect(JSON.parse(res.text)).toEqual({
             error: false,
             status: 200,
-            message: "User deleted",
-            body: SuccessfulQueryMessage.ALL_INFO_WAS_DELETE,
+            message: "Product deactivated",
+            body: "The item you wanted to update was indeed updated.",
+          });
+        });
+
+      // then we try to deactivate the very same product
+      return await request(app)
+        .delete(`/api/v1/products/${id}`)
+        .expect(500)
+        .then(async (res) => {
+          expect(JSON.parse(res.text)).toEqual({
+            error: true,
+            status: 500,
+            body: "No update was made to the product because it has the same state.",
           });
         });
     });
-  }); */
+
+    test("should return 200 if product was deactivated", async () => {
+      const productId: any = await connection.personalizedQuery(
+        `SELECT id FROM products WHERE name = '${productsReadyToCreate[0][0]}'`
+      );
+      const { id } = productId[0];
+
+      await request(app)
+        .delete(`/api/v1/products/${id}`)
+        .expect(200)
+        .then(async (res) => {
+          expect(JSON.parse(res.text)).toEqual({
+            error: false,
+            status: 200,
+            message: "Product deactivated",
+            body: "The item you wanted to update was indeed updated.",
+          });
+        });
+    });
+  });
 });
